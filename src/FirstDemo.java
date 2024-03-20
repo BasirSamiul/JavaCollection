ad8a643cb2f11a12e384aee1d2a22f83d762fcbc
SAMIUL BASIR <samiulb25@gmail.com>
2024-03-20 21:26:01 +0600

    List created

--- src/FirstDemo.java
+++ src/FirstDemo.java
@@ -1,9 +1,12 @@
+import java.util.List;
 
 public class FirstDemo {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World");
+		List<String> words = List.of("Apple", "Bannana", "Graps");
+		
+		System.out.println(words);
 
 	}
 
