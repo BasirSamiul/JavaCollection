a6a5738ffc6605680bd225482971712ad133c04a
SAMIUL BASIR <samiulb25@gmail.com>
2024-03-21 06:10:59 +0600

    List is updating

--- src/List_One.java
+++ src/List_One.java
@@ -1,9 +1,33 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
 
 public class List_One {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello world");
+		List<String> words = List.of("Banana", "Apple", "Guava");
+		
+//		Createing a Arraylist from a list
+		List<String> wordsArrayList = new ArrayList<String>(words);
+		
+//		Creating a LinkedList from a list
+		List<String> wordsLinkedList = new LinkedList<String>(words);
+		
+//		Createing a VectorList from a list
+		List<String> wordsVectorList = new Vector<String>(words);
+		
+		wordsArrayList.add("Coconut");
+		System.out.println(wordsArrayList);
+		
+		wordsLinkedList.add("Hey, I am in LinkedList");
+		System.out.println(wordsLinkedList);
+		
+		wordsVectorList.add("Hey, I am in VectorList");
+		System.out.println(wordsVectorList);
+		
+		
 		
 	}
 
