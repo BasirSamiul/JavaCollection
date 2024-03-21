19594e53da068a19d43d9ea901cbd9b980c70e76
SAMIUL BASIR <samiulb25@gmail.com>
2024-03-21 20:31:15 +0600

    Finally create the list iterator and compare the string!

--- src/List_Three.java
+++ src/List_Three.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class List_Three {
@@ -6,7 +8,37 @@ public class List_Three {
 		
 		List<String> words = List.of("Apple", "Cat", "Bat");
 		
+//		for(String word : words) {
+//			
+//			if(word.endsWith("at")) {
+//				
+//				System.out.println(word);
+//				
+//			}
+//			
+//		}
+		
 //		System.out.println(words);
+		
+		List<String> wordAl = new ArrayList<String>(words);
+		
+//		for(String word : wordAl) {
+//			if(word.endsWith("at")) {
+//				wordAl.remove(word);
+//			}
+//			System.out.println(word);
+//		}
+//		
+		
+		Iterator<String> iterator = wordAl.iterator();
+		
+		while (iterator.hasNext()) {
+			if(iterator.next().endsWith("at")) {
+				iterator.remove();
+			}
+		}
+		
+		System.out.println(wordAl);
 		
 	}
 
