7607ff9d3848f91de5df7d22383aaef9de0c835e
SAMIUL BASIR <samiulb25@gmail.com>
2024-03-21 19:19:47 +0600

    Just create a list

---
+++ src/List_Three.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class List_Three {
+
+	public static void main(String[] args) {
+		
+		List<String> words = List.of("Apple", "Cat", "Bat");
+		
+//		System.out.println(words);
+		
+	}
+
+}
